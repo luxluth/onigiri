5c429dca874897d4d46e2d909e07eef798980b3b
luxluth <luxusluth@gmail.com>
2023-02-20 22:27:58 +0100

    renderer for html (menu)

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,17 +1,38 @@
 class Item {
     label: string
     icon: string | null
-    nested: Item | null = null
+    html: HTMLDivElement | null = null
     constructor(label: string, icon: string | null=null) {
         this.label = label
         this.icon = icon
         return this
     }
 
-    nest(item: Item) {
-        this.nested = item
-        return this
+    render(): HTMLDivElement {
+        this.html = new HTMLDivElement()
+        this.html.className = "onigiri-menu-item"
+
+        if (this.icon) {
+            let ic = new HTMLDivElement()
+            let iconImage = new HTMLImageElement()
+            iconImage.src = this.icon
+            ic.append(iconImage)
+            this.html.append(ic)
+        }
+
+        let dscrp = new HTMLDivElement()
+        dscrp.innerText = this.label
+        
+        this.html.append(dscrp)
+        
+        this.html.addEventListener("click", () => {
+            // Send a custom event itemClick
+            this.html?.dispatchEvent(new CustomEvent("itemClick", { detail: this }))
+        })
+
+        return this.html
     }
+
 }
 
 class Choice {
@@ -39,11 +60,46 @@ class RangeItem extends Item {
         this.maxRange = maxRange
         this.defaultRange = defaultRange
     }
+
+    render(): HTMLDivElement {
+        this.html = new HTMLDivElement()
+        this.html.className = "onigiri-menu-item-range"
+
+        if (this.icon) {
+            let ic = new HTMLDivElement()
+            let iconImage = new HTMLImageElement()
+            iconImage.src = this.icon
+            ic.append(iconImage)
+            this.html.append(ic)
+        }
+
+        
+        let dscrp = new HTMLDivElement()
+        dscrp.innerText = this.label
+
+        let range = new HTMLInputElement()
+        range.type = "range"
+        range.min = "0"
+        range.max = this.maxRange.toString()
+        range.value = this.defaultRange.toString()
+
+        this.html.append(dscrp)
+
+        this.html.append(range)
+
+        this.html.addEventListener("click", () => {
+            // Send a custom event itemClick
+            this.html?.dispatchEvent(new CustomEvent("itemClick", { detail: this }))
+        })
+
+        return this.html
+    }
 }
 
 class ChoiceItem extends Item {
     choices: Choice[]
     currentChoice: number | null
+    activated: boolean = false
 
     constructor(
         label: string,
@@ -66,28 +122,120 @@ class ChoiceItem extends Item {
         return null
     }
 
-    setChoice(choice: number | Choice) { return choice };
+    setChoice(choice: number | Choice) {
+        if (typeof choice == "number") {
+            if (this.choices.length < choice) { this.currentChoice = choice }
+        } else {
+            this.currentChoice = this.choices.indexOf(choice)
+        }
+    }
+
+    render(): HTMLDivElement {
+        this.html = new HTMLDivElement()
+        this.html.className = "onigiri-menu-item-choice"
+
+        if (this.icon) {
+            let ic = new HTMLDivElement()
+            let iconImage = new HTMLImageElement()
+            iconImage.src = this.icon
+            ic.append(iconImage)
+            this.html.append(ic)
+        }
+
+        let dscrp = new HTMLDivElement()
+        dscrp.innerText = this.label
+
+        let choice = new HTMLDivElement()
+        choice.className = "onigiri-menu-item-choice-current"
+        let current = this.getCurrent()
+        choice.innerText = current?.label || "Unknown"
+        
+        let openIcon = new HTMLDivElement()
+        openIcon.className = "onigiri-menu-item-choice-open"
+        let openIconImage = new SVGElement()
+        openIconImage.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+        `
+        this.html.append(dscrp)
+
+        this.html.append(choice)
+        this.html.append(openIcon)
+
+        this.html.addEventListener("click", () => {
+            this.activated = true
+            let choices = new HTMLDivElement()
+            choices.className = "onigiri-menu-item-choice-choices"
+            this.choices.forEach((choice, i) => {
+                let choiceDiv = new HTMLDivElement()
+                choiceDiv.className = "onigiri-menu-item-choice-choice"
+                choiceDiv.innerText = choice.label || "Unknown"
+                choiceDiv.addEventListener("click", () => {
+                    this.currentChoice = i
+                    this.html?.dispatchEvent(new CustomEvent("itemClick", { detail: this }))
+                })
+                choices.append(choiceDiv)
+            })
+            this.html?.dispatchEvent(new CustomEvent("itemChoices", { detail: choices }))
+        })
+
+        return this.html
+
+    }
 }
 
 class ToogleItem extends Item {
     title: string = "Toogle" 
-    states: [any, any]
-    activeState: number
+    state: boolean = true 
 
     constructor(
         title: string, 
         icon: string | null = null,
-        states: [any, any], 
-        activeState: number = 0
+        state: boolean
     ) {
         super(title, icon)
         this.title = title
-        this.states = states
-        this.activeState = activeState
+        this.state = state    
     }
 
     toogle() {
-        this.activeState == 0 ? this.activeState = 1 : this.activeState = 0
+        this.state = !this.state
+    }
+
+    render(): HTMLDivElement {
+        this.html = new HTMLDivElement()
+        this.html.className = "onigiri-menu-item-toogle"
+
+        if (this.icon) {
+            let ic = new HTMLDivElement()
+            let iconImage = new HTMLImageElement()
+            iconImage.src = this.icon
+            ic.append(iconImage)
+            this.html.append(ic)
+        }
+
+        let dscrp = new HTMLDivElement()
+        dscrp.innerText = this.title
+
+        let switchElem = new HTMLLabelElement()
+        switchElem.className = "onigiri-menu-item-toogle-switch"
+        let switchInput = new HTMLInputElement()
+        switchInput.type = "checkbox"
+        switchInput.checked = this.state
+        let switchSpan = new HTMLSpanElement()
+        switchSpan.className = "onigiri-menu-item-toogle-slider"
+        switchElem.append(switchInput)
+        switchElem.append(switchSpan)
+        
+        this.html.append(dscrp)
+        this.html.append(switchElem)
+
+        this.html.addEventListener("click", () => {
+            this.toogle()
+            this.html?.dispatchEvent(new CustomEvent("itemClick", { detail: this }))
+        })
+
+        return this.html
+
     }
 }
 
@@ -99,6 +247,21 @@ class Menu {
 
     addItem(item: Item) {
         this.items = [...this.items, item] 
+    }
+
+    removeItem(item: Item) {
+        this.items = this.items.filter(i => i != item)
+    }
+
+    render(): HTMLDivElement {
+        let menu = new HTMLDivElement()
+        menu.className = "onigiri-menu"
+
+        this.items.forEach(item => {
+            menu.append(item.render())
+        })
+
+        return menu
     }
 
 }
@@ -139,10 +302,9 @@ class MenuBuilder {
     addToogle(
         title: string, 
         icon: string | null = null,
-        states: [any, any], 
-        activeState: number = 0
+        state: boolean = true
     ) {
-        this.menu.addItem(new ToogleItem(title, icon, states, activeState))
+        this.menu.addItem(new ToogleItem(title, icon, state))
         return this
     }
     
@@ -165,8 +327,6 @@ class MenuBuilder {
 }
 
 let nestedItem = new Item("top")
-nestedItem.nest(new Item("in"))
-nestedItem.nested?.nest(new Item("deeper").nest(new Item("even deeper")))
 
 let menu = new MenuBuilder()
         .addItem("lol")
